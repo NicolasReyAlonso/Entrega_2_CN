0fbfa9f58c333f66cd6685ad00c888cf017535eb
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-12-11 18:49:42 +0000

    UPDATE SCRIPT

--- second_script.sh
+++ second_script.sh
@@ -10,3 +10,10 @@ aws firehose create-delivery-stream \
   --kinesis-stream-source-configuration "RoleARN=${ROLE_ARN},KinesisStreamARN=arn:aws:kinesis:${AWS_REGION}:${ACCOUNT_ID}:stream/energy-stream" \
   --extended-s3-destination-configuration "RoleARN=${ROLE_ARN},BucketARN=arn:aws:s3:::${BUCKET_NAME},Prefix=raw/energy_consumption_five_minutes/,ErrorOutputPrefix=errors/,BufferingHints={SizeInMBs=5,IntervalInSeconds=60}"
 
+
+aws glue create-database --database-input '{"Name": "energy_database", "Description": "Database for energy consumption data"}'
+aws glue create crawler \
+    --name energy-crawler \
+    --role ${ROLE_ARN} \
+    --database-name energy_database \
+    --targets "{\"S3Targets\": [{\"Path\": \"s3://${BUCKET_NAME}/raw/energy_consumption_five_minutes/\"}]}"
